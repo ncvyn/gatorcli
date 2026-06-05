41dab38f11780019055c9e2ae148af28dde6ee5e
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-05 14:43:07 +0800

    feat: create commands.go

--- main.go
+++ main.go
@@ -13,11 +13,20 @@ func main() {
 		printErr(err)
 	}
 
-	if err := cfg.SetUser("nevan"); err != nil {
+	s := state{config: &cfg}
+	c := cli{commands: map[string]func(*state, command) error{}}
+	c.register("login", handlerLogin)
+
+	args := os.Args
+	if len(args) < 2 {
+		printErr(fmt.Errorf("missing command name"))
+	}
+
+	cmd := command{name: args[1], args: args[2:]}
+	err = c.run(&s, cmd)
+	if err != nil {
 		printErr(err)
 	}
-	fmt.Println(cfg.CurrentUserName)
-	fmt.Println(cfg.DbURL)
 }
 
 func printErr(err error) {
