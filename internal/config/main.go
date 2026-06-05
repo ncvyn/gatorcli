e2eca3bbf9e8b97073b22d49734811a8faaff662
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-05 13:36:57 +0800

    chore: move file name to const

--- internal/config/main.go
+++ internal/config/main.go
@@ -11,15 +11,16 @@ type Config struct {
 	CurrentUserName string `json:"current_user_name"`
 }
 
+const configFileName string = ".gatorconfig.json"
+
 func Read() (Config, error) {
 	homeDir, err := os.UserHomeDir()
 	if err != nil {
 		return Config{}, err
 	}
 
-	filePath := filepath.Join(homeDir, ".gatorconfig.json")
+	filePath := filepath.Join(homeDir, configFileName)
 	fileContent, err := os.ReadFile(filePath)
-
 	if err != nil {
 		return Config{}, err
 	}
