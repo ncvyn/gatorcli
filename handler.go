7873faed23c102852de411cf9b2bbf6698578fdf
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-05 21:34:06 +0800

    feat: update login command

--- handler.go
+++ handler.go
@@ -13,12 +13,17 @@ func handlerLogin(s *state, cmd command) error {
 	if len(cmd.args) < 1 {
 		return fmt.Errorf("expected at least 1 argument")
 	}
-	err := s.config.SetUser(cmd.args[0])
+
+	user, err := s.db.GetUser(context.Background(), cmd.args[0])
 	if err != nil {
 		return err
 	}
 
-	fmt.Println("User", cmd.args[0], "has been set.")
+	if err := s.config.SetUser(user.Name); err != nil {
+		return err
+	}
+
+	fmt.Println("User", user.Name, "has been logged in.")
 	return nil
 }
 
