1d5ae9dfa2fefb0e5abaafb68eccf61e7603a269
Nevan Angelo Catoy <93510179+ncvyn@users.noreply.github.com>
2026-06-06 18:12:30 +0800

    chore: make error text clearer

--- handler.go
+++ handler.go
@@ -56,7 +56,7 @@ func handlerReset(s *state, cmd command) error {
 	}
 
 	if err := s.db.ResetUsers(context.Background()); err != nil {
-		return fmt.Errorf("couldn't reset database: %w", err)
+		return fmt.Errorf("couldn't reset users: %w", err)
 	}
 	fmt.Println("Database has been reset.")
 	return nil
